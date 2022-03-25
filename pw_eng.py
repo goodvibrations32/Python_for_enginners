664242285edb21eb0850b22773b01da66a6c6d25
goodvibrations32 <tm6220@edu.hmu.gr>
2022-03-25 19:55:16 +0200

    pwd function

---
+++ pw_eng.py
@@ -0,0 +1,19 @@
+
+#%%
+from ntpath import join
+import secrets
+import string
+import random
+
+S_letters = 10
+S_digits = 8
+S_symbols = 12
+list_of_symbols = ('∗','%','@','$','(',')','[',']', '?', '#')
+
+ran = "".join(random.choices(string.ascii_lowercase, k= S_letters)+ random.choices(string.digits, k=S_digits)+ random.choices(string.punctuation, k=S_symbols))
+password =''.join( random.sample(ran, k=30))
+
+print (password)
+
+
+# %%
