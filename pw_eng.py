dc148b6c897a7e377c9ff52bc31b00f5ab88ac4a
goodvibrations32 <tm6220@edu.hmu.gr>
2022-03-26 12:08:26 +0200

    chnaged the num of symbols picked

--- pw_eng.py
+++ pw_eng.py
@@ -6,8 +6,8 @@ import string
 import random
 
 S_letters = 10
-S_digits = 8
-S_symbols = 12
+S_digits = 12
+S_symbols = 8
 list_of_symbols = ('∗','%','@','$','(',')','[',']', '?', '#')
 
 ran = "".join(random.choices(string.ascii_lowercase, k= S_letters)+ random.choices(string.digits, k=S_digits)+ random.choices(string.punctuation, k=S_symbols))
