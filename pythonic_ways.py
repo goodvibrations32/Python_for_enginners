bce843ad4678756fd72bf01e25589b3cb9303430
goodvibrations32 <tm6220@edu.hmu.gr>
2022-04-01 16:51:52 +0300

    Better for loops

---
+++ pythonic_ways.py
@@ -0,0 +1,12 @@
+#%%
+#Better way of writing a for loop in python
+
+from numpy import short
+
+stuff = [1,2,4,6,45,32,4,7,8,6,7,788]
+
+for item in stuff:
+    shorted_stuff = sorted(stuff)
+
+print (shorted_stuff)
+# %%
