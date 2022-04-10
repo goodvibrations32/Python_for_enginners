593d642a4d79c91a43809161fa634d216b4ed323
goodvibrations32 <tm6220@edu.hmu.gr>
2022-04-11 01:03:26 +0300

    reject unwanted

--- pw_eng.py
+++ pw_eng.py
@@ -1,14 +1,12 @@
 
 #%%
-from ntpath import join
-import secrets
 import string
 import random
 
 S_letters = 10
 S_digits = 12
 S_symbols = 8
-list_of_symbols = ('∗','%','@','$','(',')','[',']', '?', '#')
+#list_of_symbols = ('∗','%','@','$','(',')','[',']', '?', '#')
 
 ran = "".join(random.choices(string.ascii_lowercase, k= S_letters)+ random.choices(string.digits, k=S_digits)+ random.choices(string.punctuation, k=S_symbols))
 password =''.join( random.sample(ran, k=30))
