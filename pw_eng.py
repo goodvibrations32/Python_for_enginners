5e4ece1a31e69d707c296b876477ae10c596dfe3
goodvibrations32 <tm6220@edu.hmu.gr>
2022-05-16 21:21:35 +0300

    addition of gitignore file for pdf exersises

--- pw_eng.py
+++ pw_eng.py
@@ -11,7 +11,7 @@ S_symbols = 8
 list_of_symbols = ('∗','%','@','$','(',')','[',']', '?', '#')
 
 ran = "".join(random.choices(string.ascii_lowercase, k= S_letters)+ random.choices(string.digits, k=S_digits)+ random.choices(string.punctuation, k=S_symbols))
-password =''.join( random.sample(ran, k=30))
+password =''.join(random.sample(ran, k=30))
 
 print (password)
 
